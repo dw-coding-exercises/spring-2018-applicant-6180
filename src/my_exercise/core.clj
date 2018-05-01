167f07741065b43a6b7b08f99e05562eefd945d6
Anonymous Applicant <anonymous@example.com>
2018-05-01 14:11:21 -0700

    got something to appear on the page besides an error!

--- src/my_exercise/core.clj
+++ src/my_exercise/core.clj
@@ -1,7 +1,7 @@
 (ns my-exercise.core
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [ring.middleware.defaults :refer [wrap-defaults api-defaults]]
             [ring.middleware.reload :refer [wrap-reload]]
             [my-exercise.home :as home]))
 
